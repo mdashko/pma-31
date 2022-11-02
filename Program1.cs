939e469deb324ba521d9c1a23b0d16f817afed71
Solomiya <77298906+solik003@users.noreply.github.com>
2022-11-02 09:18:20 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        //client
+        static void Main(string[] args)
+        {
+            connection:
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 1302);
+                string messageToSend = "My name is Solia";
+
+                int byteCount = Encoding.ASCII.GetByteCount(messageToSend + 1);
+                byte[] sendData = new byte[byteCount];
+                sendData = Encoding.ASCII.GetBytes(messageToSend);
+
+                NetworkStream stream = client.GetStream();
+                stream.Write(sendData, 0, sendData.Length);
+                Console.WriteLine("sending data to server...");
+
+                StreamReader sr = new StreamReader(stream);
+                string response = sr.ReadLine();
+                Console.WriteLine(response);
+
+                stream.Close();
+                client.Close();
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("failed to connect");
+                goto connection;
+            }
+        }
+    }
+}
